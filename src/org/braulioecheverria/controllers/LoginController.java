1efaa47dc364066a3e3bc99aa7457bee3e9940e0
Braulio Echeverria <braulioecheverria@kinal.org.gt>
2025-07-29 20:25:01 -0600

    register done

--- src/org/braulioecheverria/controllers/LoginController.java
+++ src/org/braulioecheverria/controllers/LoginController.java
@@ -7,8 +7,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
--- src/org/braulioecheverria/controllers/NewUserController.java
+++ src/org/braulioecheverria/controllers/NewUserController.java
@@ -1,17 +1,34 @@
 package org.braulioecheverria.controllers;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.CallableStatement;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.braulioecheverria.dao.Conexion;
+import org.braulioecheverria.models.Persona;
+import org.braulioecheverria.models.Usuario;
 import org.braulioecheverria.system.Main;
+
+// 1, agregar persona, buscar persona, agregar usuario
 
 public class NewUserController implements Initializable {
     private Main escenarioPrincipal;
+    private Persona nuevaPersona = new Persona();
+    private Usuario nuevoUsuario = new Usuario();
     
     @FXML
     private TextField txtNombres, txtApellidos, txtTelefono, txtEmail;
@@ -35,8 +52,87 @@ public class NewUserController implements Initializable {
         this.escenarioPrincipal = escenarioPrincipal;
     }
     
+    private void limpiarControles(){
+        txtNombres.setText("");
+        txtApellidos.setText("");
+        txtTelefono.setText("");
+        txtEmail.setText("");
+        txtPassword.setText("");
+    }
+    
+    private void agregarPersona(){
+        nuevaPersona.setNombres(txtNombres.getText());
+        nuevaPersona.setApellidos(txtApellidos.getText());
+        nuevaPersona.setTelefono(txtTelefono.getText());
+        try{
+            PreparedStatement sp = Conexion.getInstance()
+                    .getConexion()
+                    .prepareCall("CALL sp_agregar_persona(?,?,?);");
+            sp.setString(1, nuevaPersona.getNombres());
+            sp.setString(2, nuevaPersona.getApellidos());
+            sp.setString(3, nuevaPersona.getTelefono());
+            sp.execute();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    private String idPersona(){
+        String id = null;
+        try{
+            CallableStatement cs = Conexion.getInstance()
+                    .getConexion()
+                    .prepareCall("{CALL sp_buscar_persona(?)}");
+            cs.registerOutParameter(1, java.sql.Types.VARCHAR);
+            cs.execute();
+            id = cs.getString(1);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return id;
+    }
+    
+    private void agregarUsuario(String idPersona){
+        nuevoUsuario.setEmail(txtEmail.getText());
+        nuevoUsuario.setPassword(txtPassword.getText());
+        nuevoUsuario.setIdPersona(idPersona);
+        try{
+            PreparedStatement sp = Conexion.getInstance()
+                    .getConexion()
+                    .prepareCall("CAll sp_agregar_usuario(?,?,?);");
+            sp.setString(1, nuevoUsuario.getEmail());
+            sp.setString(2, nuevoUsuario.getPassword());
+            sp.setString(3, nuevoUsuario.getIdPersona());
+            sp.execute();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
     @FXML
     public void eventoAceptar(ActionEvent event){
+        agregarPersona();
+        String id = idPersona();
+        agregarUsuario(id);
+        Alert alerta = new Alert(AlertType.INFORMATION);
+        alerta.setTitle("Registro de Usuarios");
+        alerta.setHeaderText("Éxito!!!");
+        alerta.setContentText("El registro se realizó de manera correcta");
+        alerta.showAndWait();
+        limpiarControles();
+        Alert alerta2 = new Alert(AlertType.WARNING, "¿Desesas iniciar sesión?",
+                        ButtonType.YES, ButtonType.NO);
+        Optional<ButtonType> seleccion = alerta2.showAndWait();
+        if(seleccion.get() == ButtonType.YES){
+            escenarioPrincipal.login();
+        }else{
+            Alert alerta3 = new Alert(AlertType.INFORMATION);
+            alerta3.setTitle("Registro de Usuarios");
+            alerta3.setHeaderText("Adiós");
+            alerta3.setContentText("Gracias por utilizar mi programa");
+            alerta3.showAndWait();
+            Platform.exit();
+        }
         
     }
     
